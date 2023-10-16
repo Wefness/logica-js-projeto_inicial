afd4ba0122e69db061c0975fba62524d5f16653e
Wefness <affonsomiguel@hotmail.com>
2023-10-16 13:40:56 -0300

    adicionado else e colocado na mensagem a variavel do numero secreto

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
+
 let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
 
+//se chute for igual ao numero secreto
 if(chute == numeroSecreto){
-    console.log('Isso ai! Você descobriu o número secreto (5)')
+    alert('Isso ai! Você descobriu o número secreto ('+ chute +')')
+} else {
+    alert('Você errou :(')
 }
