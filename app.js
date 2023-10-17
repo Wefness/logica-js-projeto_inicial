154f5027c9deab91dc751ea5bd02a444c2790677
Wefness <affonsomiguel@hotmail.com>
2023-10-17 13:21:31 -0300

    numero secreto gerado aleatoriamente adicionado quantidade de tentativas adicionado mensagem no log informando numero secreto e tentativas realizadas logica while implementada para continuar o jogo enquanto o numero secreto nao for descoberto adicionado mensagens informando se o chute e maior ou menor que o numero secreto

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 0;
+let numeroSecreto = Math.floor((Math.random()*10)+1);
 
-//se chute for igual ao numero secreto
-if(chute == numeroSecreto){
-    alert('Isso ai! Você descobriu o número secreto ('+ chute +')')
-} else {
-    alert('Você errou :(')
+//enquanto chute nao for igual ao numeroSecreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    tentativas++;
+
+    //informa no console o numero secreto e a tentativa atual
+    console.log(`Número secreto: ${numeroSecreto}`);
+    console.log(`Tentativas: ${tentativas}`);
+
+    //se chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número é menor que ${chute}`);
+        } else {
+            alert(`O número é maior que ${chute}`);
+        }
+
+        //limpa console para nova tentativa
+        console.clear();
+    }
 }
